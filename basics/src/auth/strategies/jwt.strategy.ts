f54a5f067e099c47c0fc6f24aac55c50afd8b3ea
jeffngugi <ngugimwathi254@gmail.com>
2026-01-03 16:55:05 +0300

    fix: use guards to protect routes

---
+++ basics/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable } from '@nestjs/common';
+import * as config from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import jwtConfig from '../config/jwt.config';
+import { AuthJwtPayload } from '../types/auth-jwtPayload';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: config.ConfigType<typeof jwtConfig>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtConfiguration.secret as string,
+    });
+  }
+
+  validate(payload: AuthJwtPayload) {
+    return { id: payload.sub };
+  }
+}
---
+++ basics/src/user/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectRepository(User) private UserRepo: Repository<User>) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = this.UserRepo.create(createUserDto);
+    return await this.UserRepo.save(user);
+  }
+
+  async findByEmail(email: string) {
+    return await this.UserRepo.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  async findOne(id: number) {
+    return this.UserRepo.findOne({
+      where: { id },
+      select: ['firstName', 'lastName', 'avatarUrl'],
+    });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
